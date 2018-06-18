653520f448d29bd28c34f06f225387025cd10df5
Tiago Dávila <tdavilas@gmail.com>
2018-06-18 08:33:44 -0300

    Adding new headers properties
    Those have no incidence of CSS. Ugly as hell.

--- src/general/Header.js
+++ src/general/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
+import HeaderTitle from "./HeaderTitle";
+import DellarcoCarousel from "./DellarcoCarousel";
 
 class Header extends Component {
   render() {
@@ -8,6 +10,15 @@ class Header extends Component {
         <Col>
           <Row>
             <Col>Aqui vai o logo</Col>
+          </Row>
+          <Row>
+            <Col className="text-right"><HeaderTitle/></Col>
+          </Row>
+          <Row>
+            <Col>Aqui o form do eduardo</Col>
+          </Row>
+          <Row>
+            <Col><DellarcoCarousel /></Col>
           </Row>
         </Col>
       </Row>
---
+++ src/general/HeaderTitle.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HeaderTitle extends Component {
+  render() {
+    return (
+      <h2>Lorem <b>ipsum</b> dolor sit amet et noia</h2>
+    );
+  }
+}
+
+export default HeaderTitle;
