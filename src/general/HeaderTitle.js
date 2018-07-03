00404362bab757eb2d6bccbf7502acd29cd1f8fa
Marcelo Junior <marceloadsj1@gmail.com>
2018-07-03 20:47:54 -0300

    Moved Header Title and formatted with css classes

--- src/general/HeaderTitle.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class HeaderTitle extends Component {
-  render() {
-    return <h2>Conheça a Experiência Dell'Arco</h2>;
-  }
-}
-
-export default HeaderTitle;
---
+++ src/header/HeaderTitle.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
+
+class Title extends Component {
+  render() {
+    return (
+      <Row className="mb-5 mr-5 text-right text-white">
+        <Col>
+          <h2>Conheça a Experiência Dell'Arco</h2>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Title;
